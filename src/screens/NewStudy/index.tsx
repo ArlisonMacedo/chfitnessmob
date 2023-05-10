355938a9787361e6f04219636f38860368964341
ArlisonMacedo <arlisonmacedo526@gmail.com>
2023-05-10 11:07:27 -0300

    half finish screen create user

--- src/screens/NewStudy/index.tsx
+++ src/screens/NewStudy/index.tsx
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { View, Text, TextInput, TouchableHighlight } from "react-native";
 import Header from "../../components/Header";
 
 
@@ -7,6 +7,33 @@ export default function NewStudy() {
     return (
         <View style={styles.container}>
             <Header />
+
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    Entre com os dados do aluno
+                </Text>
+                <View style={styles.form}>
+                    <Text style={styles.label}>Nome</Text>
+                    <TextInput
+                        placeholder="Ex. Rana Ribeiro"
+                        placeholderTextColor="#c4c4cc"
+                        autoCapitalize="words"
+                        style={styles.inputText}
+                    />
+                    <Text style={styles.label}>WhatsApp</Text>
+                    <TextInput
+                        keyboardType="numeric"
+                        placeholder="Ex. 9898888-2222"
+                        placeholderTextColor="#c4c4cc"
+                        autoCapitalize="characters"
+                        style={styles.inputText}
+                    />
+                    <TouchableHighlight style={styles.button} activeOpacity={1} underlayColor="#c4c4cc" onPress={() => alert('Pressed')}>
+                        <Text style={styles.buttonText}>Cadastrar</Text>
+                    </TouchableHighlight>
+                </View>
+            </View>
+
         </View>
     )
 }
--- src/screens/NewStudy/styles.ts
+++ src/screens/NewStudy/styles.ts
@@ -5,5 +5,50 @@ export const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#0A3F53'
+    },
+    content: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 0.9
+    },
+    title: {
+        fontFamily: 'Nunito_700Bold',
+        fontSize: 22,
+        color: '#fff'
+    },
+    form: {
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: 40
+        // height: 70
+    },
+    label: {
+        color: '#FBEF55',
+        fontFamily: 'Nunito_700Bold',
+        alignSelf: 'center',
+        marginBottom: 10,
+    },
+    inputText: {
+        height: 50,
+        backgroundColor: '#FFF',
+        marginBottom: 20,
+        width: '70%',
+        borderRadius: 5,
+        fontFamily: 'Nunito_400Regular',
+        paddingHorizontal: 10
+    },
+    button: {
+        width: '70%',
+        height: 50,
+        backgroundColor: '#FBEF55',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5
+    },
+    buttonText: {
+        color: '#000',
+        fontFamily: 'Nunito_700Bold',
+        fontSize: 17
     }
 })
